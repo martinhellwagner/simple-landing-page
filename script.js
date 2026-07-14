2024eade0b4d780ac890e8910699fc92a5123c7f
Martin Hellwagner <martin.hellwagner@dynatrace.com>
2026-07-14 11:06:46 +0200

    Implement mobile improvements

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const breakpointMedium = 768;
-const breakpointMax = 1200;
+// Reload on orientation change
+window.addEventListener('orientationchange', () => {
+  window.location.reload();
+});
 
-calculateHeight = () => {
-  const container = document.querySelector('.container');
+// Set container height
+const container = document.querySelector('.container');
+const height = Math.max(window.innerHeight, container.getBoundingClientRect().height);
 
-  document.body.style.setProperty('--windowHeight', `${window.innerHeight}px`);
-  container.style.setProperty('--windowHeight', `${window.innerHeight}px`);
-  container.classList.add('container--ready');
+document.body.style.setProperty('--windowHeight', `${height}px`);
+container.style.setProperty('--windowHeight', `${height}px`);
 
-  initSnow();
-}
+// Show images on load
+const images = container.querySelectorAll('img');
+let loadedImages = 0;
 
-calculateHeight();
+images.forEach(() => {
+  loadedImages++;
+
+  if (images.length === loadedImages) {
+    container.classList.add('container--ready');
+  }
+});
